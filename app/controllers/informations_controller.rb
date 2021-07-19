1d3f8c34b40ed7ba96af2a9c2b99129b1c2f4c2d
a <akiizu.2612@gmail.com>
2021-07-19 17:28:56 +0900

    貸出ビュー作成

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,2 +1,20 @@
 class InformationsController < ApplicationController
+    def index
+        @msg = "貸出中一覧"
+        @data = Information.all
+    end
+
+    def returned
+        @msg = "返却済み一覧"
+        @data = Information.all
+    end
+
+    def kasidashi
+        @msg = "貸出手続き"
+        @information = Information.new
+    end
+
+    def add
+        
+    end
 end
